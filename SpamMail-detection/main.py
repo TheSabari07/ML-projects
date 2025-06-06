75f0c2592172e85fc73d3b54081abc78d515d08b
Sabari Doss R <sabursd18@gmail.com>
2025-06-06 18:13:21 +0530

    check : checking dataset

--- SpamMail-detection/main.py
+++ SpamMail-detection/main.py
@@ -7,3 +7,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
+df = pd.read_csv('data/spam.csv', encoding='latin-1')
+
+
+
+# Keep only the important columns
+df = df[['v1', 'v2']]
+df.columns = ['label', 'message']  # Rename for clarity
+
+# Check shape and info
+print(df.shape)
+print(df.info())
